f8f78f1924f1cb9c9e064b8708887edcd1358a57
Paulo Peleteiro <paulop@gmail.com>
2022-05-27 11:33:46 -0300

    Ex05

---
+++ Exercise005-diagonal-difference.py
@@ -0,0 +1,16 @@
+ # Write your code here
+    leftDiagonalSum, rightDiagonalSum = 0, 0
+    arr_len = len(arr)
+    i = 0
+    j = 0
+    while( i < arr_len):
+        leftDiagonalSum += arr[i][j]
+        i += 1 
+        j += 1
+        
+    i = 0
+    j = arr_len - 1
+    while( i < arr_len):
+        rightDiagonalSum += arr[i][j]
+        i += 1 
+        j -= 1
